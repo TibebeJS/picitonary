1e145a996fb06634f4f71dcaf37bd77d9c2b94ce
TibebeJs <Tibebes.js@gmail.com>
2022-01-06 12:36:12 +0300

    🗯 chore(vue): add tailwindCSS

--- packages/web-ui/src/components/HelloWorld.vue
+++ packages/web-ui/src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-3xl font-bold underline">
+    {{msg}}
+  </h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
